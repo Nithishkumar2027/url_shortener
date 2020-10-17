fc19222c9a45e091978e1a2cf4caa7352fff3bab
Nithish kumar M.N <mnnithishkumarbtech@gmail.com>
2020-10-17 23:49:54 +0530

    Created async functionality to create Short URL

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express')
+const mongoose  = require('mongoose')
 const morgan = require('morgan')
-
+const ShortUrl = require('./models/shortUrl')
 const app = express()
+
+const mongoURI = 'Your Mongo URL here'
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 app.use(morgan('dev'))
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/',async (req, res) => {
+    const shorturls = await ShortUrl.find()
+    res.render('index', {shorturls: shorturls})
+})
+
+app.post('/shortUrls',async (req,res) => {
+    await ShortUrl.create({ full: req.body.fullUrl})
+    res.redirect('/')
 })
 
 const port = process.env.PORT || 3000
